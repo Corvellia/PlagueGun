45aa438890727ded093968e05fa868bb5318e82e
Albert <ajberger2013@gmail.com>
2023-09-23 15:13:22 -0500

    Created the Projectile_PlagueBullet.  This should tie to the XML tag "thingClass" at the end of the first ThingDef containing the Plague Bullet.

---
+++ Source/PlagueGun/PlagueGun/Projectile_PlagueBullet.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plague
+{
+    public class Projectile_PlagueBullet : Bullet
+    {
+        #region Properties
+
+        public ThingDef_PlagueBullet Def
+        {
+            get
+            {
+                return this.def as ThingDef_PlagueBullet;
+            }
+        }
+
+        #endregion Properties
+    }
+}
